f701529e41e7f5a41ca886e2654fc55b5ac1a720
reidfluegel <reid.fluegel@gmail.com>
2025-04-29 22:10:24 -0400

    hello world endpoint

--- src/main/groovy/org/example/Application.groovy
+++ src/main/groovy/org/example/Application.groovy
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class Application {
     static void main(String[] args) {
-        println "Hello world!"
         SpringApplication.run(Application, args)
     }
 }
