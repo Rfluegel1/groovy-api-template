f8bf1aecd5c425ac6ea2c5312a995217cc6dc2ae
reidfluegel <reid.fluegel@gmail.com>
2025-05-28 09:56:06 -0500

    enable stage tests locally

--- src/test/groovy/org/example/HealthCheckFT.groovy
+++ src/test/groovy/org/example/HealthCheckFT.groovy
@@ -1,30 +1,28 @@
 package org.example
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ContextConfiguration
-import spock.lang.Ignore
-import spock.lang.IgnoreIf
 import spock.lang.Specification
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = Application)
 class HealthCheckFT extends Specification {
 
-    @Autowired
-    TestRestTemplate restTemplate
+    @Value('${base.url}')
+    String baseUrl
+
+    def restTemplate = new TestRestTemplate()
 
     def "health check returns app integrations"() {
         when:
-        def response = restTemplate.getForEntity("/health-check", Map)
+        def response = restTemplate.getForEntity("${baseUrl}/health-check", Map)
 
         then:
         response.statusCode == HttpStatus.OK
-
         response.body.result == 'success'
-        response.body.integrations.size() == 1
         response.body.integrations.find({ it.name == 'pocketbase' }).result == 'success'
     }
 }
--- src/test/groovy/org/example/HeartbeatFT.groovy
+++ src/test/groovy/org/example/HeartbeatFT.groovy
@@ -1,22 +1,24 @@
 package org.example
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = Application)
 class HeartbeatFT extends Specification {
 
-    @Autowired
-    TestRestTemplate restTemplate
+    @Value('${base.url}')
+    String baseUrl
+
+    def restTemplate = new TestRestTemplate()
 
     def "heartbeat returns app version"() {
         when:
-        def response = restTemplate.getForEntity("/heartbeat", Map)
+        def response = restTemplate.getForEntity("${baseUrl}/heartbeat", Map)
 
         then:
         response.statusCode == HttpStatus.OK
